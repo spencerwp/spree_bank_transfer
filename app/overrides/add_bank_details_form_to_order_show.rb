b11ff6c5a53b31ffcba3c02822985b6d43f9e1dd
spencerwp <sppow93@gmail.com>
2021-02-23 00:36:17 -0500

    Update add_bank_details_form_to_order_show.rb

--- app/overrides/add_bank_details_form_to_order_show.rb
+++ app/overrides/add_bank_details_form_to_order_show.rb
@@ -1,6 +1,6 @@
 Deface::Override.new(
   virtual_path: 'spree/orders/show',
   name: 'add_bank_details_form_to_order_show',
-  insert_bottom: ".checkout-confirm-delivery-informations",
+  insert_bottom: "[data-hook='order-payment']",
   partial: "spree/orders/bank_transfer_form"
 )
